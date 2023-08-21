3d4b15a6679f8f4e08306d2aea9126b77ca91306
Amanullah Menjli <menjliamanullah@gmail.com>
2023-08-21 08:15:47 +0100

    it works now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), svelte()],
+  experimental: {
+    assets: true
+  }
 });
