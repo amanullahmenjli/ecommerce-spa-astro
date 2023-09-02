f5c42d3b7197460cab5b84cae4c27af140103e00
Amanullah Menjli <menjliamanullah@gmail.com>
2023-09-03 00:28:11 +0100

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,13 +12,12 @@ module.exports = {
     ],
     theme: {
       },
-      darkMode: "class",
 
     plugins: [nextui({
       themes: {
         light: {
           colors: {
-            background: "#FFFFFF", // or DEFAULT
+            background: "#EDF2FF", // or DEFAULT
             foreground: "#11181C", // or 50 to 900 DEFAULT
             primary: {
               //... 50 to 900
@@ -30,4 +29,14 @@ module.exports = {
         }
       },
     }), require("daisyui")],
+    daisyui: {
+      themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+      darkTheme: "dark", // name of one of the included themes for dark mode
+      base: false, // applies background color and foreground color for root element by default
+      styled: true, // include daisyUI colors and design decisions for all components
+      utils: true, // adds responsive and modifier utility classes
+      rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+      prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+      logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    },
 }
